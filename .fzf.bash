3e7544e25067e348e386600bc3528ea438434af5
Ben Ashby-Wood <58435096+bba7474@users.noreply.github.com>
2024-04-12 15:15:06 +0100

    Add fzf config

---
+++ .fzf.bash
@@ -0,0 +1,10 @@
+eval "$(fzf --bash)"
+
+export FZF_CTRL_T_OPTS="
+  --preview 'batcat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --color header:italic"
+ export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
--- README.md
+++ README.md
@@ -6,6 +6,9 @@ README
 
 * K8s
 * SDKMan
+* fzf
+* bat
+* tree
 
 #ZSH
 For help in setting up ZSH files on Macbooks, follow (this guide)[https://www.freecodecamp.org/news/jazz-up-your-zsh-terminal-in-seven-steps-a-visual-guide-e81a8fd59a38/]
